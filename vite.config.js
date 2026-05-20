c17919dff0eac05150bcbd92c1be3d26b48aa270
brayan <brayanalvcod@gmail.com>
2026-05-19 19:42:17 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3001,
     strictPort: true,
-    allowedHosts: ['manga.brayanalvz.xyz'],
+    allowedHosts: ['portfolio.brayanalvz.xyz'],
   },
 })
